1ecdd452ab25566323065bb120fe317459ad72cc
xClaudiaArias <xClaudiaArias@gmail.com>
2024-01-06 13:39:43 -0500

    navigation complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
 import  React, { useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
-import CategoriesNav from './Components/CategoriesNav/CategoriesNav';
+import Home from './Pages/Home';
+import ShopCategory from './Pages/ShopCategory';
+import Cart from './Pages/Cart';
+import Account from './Pages/Account';
+import Product from './Pages/Product';
+import Wishlist from './Pages/Wishlist';
+import Search from './Pages/Search';
+
 
 
 const App = () => {
@@ -11,8 +18,28 @@ const App = () => {
 
     return (
         <>
-            <Navbar />
-            <CategoriesNav />
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home/>} />
+                    <Route path="/search" element={<Search />} />
+                    <Route path="/wishlist" element={<Wishlist />} />
+                    <Route path="/babies" element={<ShopCategory category="babies"/>} />
+                    <Route path="/toddlers" element={<ShopCategory category="toddlers"/>} />
+                    <Route path="/kids" element={<ShopCategory category="kids"/>} />
+                    <Route path="/accessories" element={<ShopCategory category="accessories"/>} />
+
+                    <Route path="/product" element={<Product />}>
+                        <Route path=":productId" element={<Product />} />
+                    </Route>
+
+                    <Route path="/cart" element={<Cart />} />
+
+                    <Route path="/account" element={<Account />} />
+                </Routes>
+            
+            </BrowserRouter>
+
         </>
     )
 }
--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -6,29 +6,49 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
 
+    const [menu, setMenu] = useState("home")
+    const [empty, setEmpty] = useState("0")
+
     return (
-        <div className='navbar'>
-            <ul className='navMenu'>
-                <li> <Link style={{textDecoration: 'none', textTransform: 'uppercase', color: 'white'}} to="/wishlist">Wishlist</Link></li>
-                <li> <Link style={{textDecoration: 'none', textTransform: 'uppercase', color: 'white'}} to="/account">Account</Link></li>
+        <>
 
-                <li className='nav-logo'> 
-                    <Link style={{textDecoration: 'none', textTransform: 'uppercase', color: 'white'}} to="/">
-                        <div>-IMG-</div>
-                        <p>Chiquita</p>
-                    </Link>
-                </li>
+            <div className='navbar'>
+                <ul className='navMenu'>
+                    <li> <Link className={menu === "wishlist" ? '' : ''} onClick={() => setMenu("wishlist")} style={{textDecoration: 'none', textTransform: 'uppercase', color: 'white'}} to="/wishlist">Wishlist</Link></li>
+                    <li> <Link className={menu === "account" ? '' : ''} onClick={() => setMenu("account")} style={{textDecoration: 'none', textTransform: 'uppercase', color: 'white'}} to="/account">Account</Link></li>
+
+                    <li className='nav-logo'> 
+                        <Link className={menu === "home" ? '' : ''} onClick={() => setMenu("home")} style={{textDecoration: 'none', textTransform: 'uppercase', color: 'white'}} to="/">
+                            <div>-IMG-</div>
+                            <p>Chiquita</p>
+                        </Link>
+                    </li>
 
 
-                <li> <Link style={{textDecoration: 'none', textTransform: 'uppercase', color: 'white'}} to="/search">Search</Link></li>
-                <li>
-                    <div className="nav-cart">
-                        <Link style={{textDecoration: 'none', textTransform: 'uppercase', color: 'white'}} to="/cart">Cart</Link>
-                        <div className="nav-cart-count">0</div>
-                    </div>
-                </li>
-            </ul>
-        </div>
+                    <li> <Link className={menu === "search" ? '' : ''} onClick={() => setMenu("search")} style={{textDecoration: 'none', textTransform: 'uppercase', color: 'white'}} to="/search">Search</Link></li>
+                    <li>
+                        <div className="nav-cart">
+                            <Link className={menu === "cart" ? '' : ''} onClick={() => setMenu("cart")} style={{textDecoration: 'none', textTransform: 'uppercase', color: 'white'}} to="/cart">Cart</Link>
+                            <div className={empty === "0" ? 'empty' : 'nav-cart-count'} onLoad={() => setEmpty("0")}></div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
+
+            <div className='categories-nav'>
+                <ul className='categories-nav-menu'>
+                    <li> <Link className={menu === "babies" ? 'categories-nav-item' : ''} onClick={() => setMenu("babies")} style={{textDecoration: 'none'}} to="/babies">Babies</Link></li>
+
+
+
+                    <li> <Link  className={menu === "toddlers" ? 'categories-nav-item' : ''} onClick={() => setMenu("toddlers")} style={{textDecoration: 'none'}} to="/toddlers">Toddlers</Link></li>
+                    <li> <Link className={menu === "kids" ? 'categories-nav-item' : ''} onClick={() => setMenu("kids")} style={{textDecoration: 'none'}} to="/kids">Kids</Link></li>
+                    <li> <Link className={menu === "accessories" ? 'categories-nav-item' : ''} onClick={() => setMenu("accessories")} style={{textDecoration: 'none'}} to="/accessories">Accessories</Link></li>
+                </ul>
+            </div>
+        </>
+        
     )
 }
 
