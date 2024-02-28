7bb8e63ca2bc804639b8ce0f1917ddfac8302a59
xClaudiaArias <xClaudiaArias@gmail.com>
2024-02-28 13:49:06 -0500

    admin

---
+++ admin/src/pages/Admin/Admin.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Admin.css'
+
+const Admin = () => {
+    return (
+        <div className='admin'>
+            Admin
+        </div>
+    )
+}
+
+export default Admin
